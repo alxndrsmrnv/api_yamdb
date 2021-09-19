dfe73657a57e90b5e5540225ae6c063d41ff137f
Alexander <ya.alexandersmirnov@yandex.ru>
2021-09-19 18:24:52 +0300

    review

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -7,7 +7,6 @@ from .views import (CategoriesViewSet, CreateProfileView, GenresViewSet,
                     TokenView)
 
 router_v1 = DefaultRouter()
-#router_v1.register(r'users/(?P<username>\d+)', ProfileViewSet)
 router_v1.register(r'categories', CategoriesViewSet)
 router_v1.register(r'genres', GenresViewSet)
 router_v1.register(r'titles', TitlesViewSet)
